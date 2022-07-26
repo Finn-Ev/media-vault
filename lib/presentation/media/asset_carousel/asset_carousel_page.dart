dbf8f4fbadb4a0674885955c3a74f3ab8d620663
finn-ev <finnevermann@gmx.de>
2022-07-27 00:21:59 +0200

    fix/error when deleting the last element of the album when viewed in carousel

--- lib/presentation/media/asset_carousel/asset_carousel_page.dart
+++ lib/presentation/media/asset_carousel/asset_carousel_page.dart
@@ -1,3 +1,4 @@
+import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:media_vault/application/assets/asset_carousel/asset_carousel_bloc.dart';
@@ -27,47 +28,58 @@ class AssetCarouselPage extends StatelessWidget {
         BlocProvider<AssetControllerBloc>(create: (context) => sl<AssetControllerBloc>()),
         BlocProvider<AssetCarouselBloc>(create: (context) => sl<AssetCarouselBloc>())
       ],
-      child: BlocBuilder<AssetObserverBloc, AssetObserverState>(
+      child: BlocConsumer<AssetObserverBloc, AssetObserverState>(
+        listener: (context, state) {
+          if (state is AssetObserverLoaded) {
+            if (state.assets.isEmpty) {
+              AutoRouter.of(context).pop();
+            }
+          }
+        },
         builder: (context, assetObserverState) {
           if (assetObserverState is AssetObserverInitial || assetObserverState is AssetObserverLoading) {
             return const LoadingIndicator();
           }
           if (assetObserverState is AssetObserverLoaded) {
-            return GestureDetector(
-              behavior: HitTestBehavior.opaque,
-              onTap: () {
-                BlocProvider.of<AssetCarouselBloc>(context).add(ToggleUI());
-              },
-              child: Scaffold(
-                body: BlocBuilder<AssetCarouselBloc, AssetCarouselState>(
-                  builder: (context, state) {
-                    if (assetObserverState.assets.isEmpty) {
-                      return const Center(child: Text('No assets found'));
-                    }
+            if (assetObserverState.assets.isEmpty) {
+              print('AssetCarouselPage: assets is empty');
+              return const Scaffold();
+            } else {
+              return GestureDetector(
+                behavior: HitTestBehavior.opaque,
+                onTap: () {
+                  BlocProvider.of<AssetCarouselBloc>(context).add(ToggleUI());
+                },
+                child: Scaffold(
+                  body: BlocBuilder<AssetCarouselBloc, AssetCarouselState>(
+                    builder: (context, state) {
+                      if (assetObserverState.assets.isEmpty) {
+                        return Container();
+                      }
+                      // if the carousel-index exceeds the number of assets (e.g. when an asset has been moved or deleted),
+                      // it will be set to last element of the array.
+                      final Asset currentAsset = state.carouselIndex >= assetObserverState.assets.length
+                          ? assetObserverState.assets[assetObserverState.assets.length - 1]
+                          : assetObserverState.assets[state.carouselIndex];
 
-                    // if the carousel-index exceeds the number of assets (e.g. when an asset has been moved or deleted),
-                    // it will be set to last element of the array.
-                    final Asset currentAsset = state.carouselIndex >= assetObserverState.assets.length
-                        ? assetObserverState.assets[assetObserverState.assets.length - 1]
-                        : assetObserverState.assets[state.carouselIndex];
-
-                    final bool isLastAsset = state.carouselIndex == assetObserverState.assets.length - 1;
-                    return SafeArea(
-                      child: Stack(
-                        children: [
-                          AssetCarousel(
-                            albumId: albumId,
-                            initialAssetId: initialAssetId,
-                          ),
-                          state.showMenuUI ? const AssetCarouselTopMenu() : Container(),
-                          state.showMenuUI ? AssetCarouselBottomMenu(albumId: albumId, currentAsset: currentAsset, lastAlbumAssetIsViewedInCarousel: isLastAsset) : Container(),
-                        ],
-                      ),
-                    );
-                  },
+                      final bool isLastAsset = state.carouselIndex == assetObserverState.assets.length - 1;
+                      return SafeArea(
+                        child: Stack(
+                          children: [
+                            AssetCarousel(
+                              albumId: albumId,
+                              initialAssetId: initialAssetId,
+                            ),
+                            state.showMenuUI ? const AssetCarouselTopMenu() : Container(),
+                            state.showMenuUI ? AssetCarouselBottomMenu(albumId: albumId, currentAsset: currentAsset, lastAlbumAssetIsViewedInCarousel: isLastAsset) : Container(),
+                          ],
+                        ),
+                      );
+                    },
+                  ),
                 ),
-              ),
-            );
+              );
+            }
           } else {
             return const Center(child: Text('Error'));
           }
