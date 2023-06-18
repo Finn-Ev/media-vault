7b3b1074fff1760fde77b466155f02b828ca0ff8
Finn Evermann <finn.evermann@icloud.com>
2023-06-18 20:20:15 +0200

    auto logout user when no auth-state gets emitted after app launch

--- lib/presentation/other/splash/splash_page.dart
+++ lib/presentation/other/splash/splash_page.dart
@@ -8,6 +8,19 @@ import 'package:media_vault/presentation/_widgets/loading_indicator.dart';
 class SplashPage extends StatelessWidget {
   const SplashPage({Key? key}) : super(key: key);
 
+  void _showSnackbar(BuildContext context) {
+    ScaffoldMessenger.of(context).showSnackBar(
+      const SnackBar(
+        backgroundColor: Colors.red,
+        content: Text(
+          'Unfortunately, an issue occurred while trying to authenticate you. Please log in again.',
+          style: TextStyle(color: Colors.white, fontSize: 16),
+        ),
+        duration: Duration(seconds: 5),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return BlocListener<AuthCoreBloc, AuthCoreState>(
@@ -18,9 +31,23 @@ class SplashPage extends StatelessWidget {
           AutoRouter.of(context).replace(const LocalAuthRootRoute());
         }
       },
-      child: const Scaffold(
+
+      // log out user when no AuthState was emitted after 8 seconds
+      child: Scaffold(
         body: Center(
-          child: LoadingIndicator(),
+          child: FutureBuilder(
+            future: Future.delayed(const Duration(seconds: 8)),
+            builder: (context, snapshot) {
+              if (snapshot.connectionState == ConnectionState.done) {
+                // display hint that there was an issue with the authentication and that the user should log in again
+                WidgetsBinding.instance.addPostFrameCallback((_) => _showSnackbar(context));
+
+                BlocProvider.of<AuthCoreBloc>(context).add(LogoutRequested());
+                AutoRouter.of(context).replace(const LoginRoute());
+              }
+              return const LoadingIndicator();
+            },
+          ),
         ),
       ),
     );
