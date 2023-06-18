2a0f89f47599b384588ca6a446825270ee404329
Finn Evermann <finn.evermann@icloud.com>
2023-06-18 17:11:48 +0200

    only scroll to the bottom of the asset-list when the page is first loaded after the controller added assets to the asset-list

--- lib/presentation/media/asset_list/widgets/asset_list.dart
+++ lib/presentation/media/asset_list/widgets/asset_list.dart
@@ -4,6 +4,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:media_vault/application/assets/asset_list/asset_list_bloc.dart';
+import 'package:media_vault/application/assets/controller/asset_controller_bloc.dart';
 import 'package:media_vault/application/assets/observer/asset_observer_bloc.dart';
 import 'package:media_vault/infrastructure/repositories/asset_repository_impl.dart';
 import 'package:media_vault/presentation/_widgets/loading_indicator.dart';
@@ -39,101 +40,108 @@ class _AssetListState extends State<AssetList> {
   @override
   Widget build(BuildContext context) {
     final themeData = Theme.of(context);
-    return BlocBuilder<AssetObserverBloc, AssetObserverState>(
-      builder: (context, assetObserverState) {
-        if (assetObserverState is AssetObserverInitial) {
-          return Container();
-        } else if (assetObserverState is AssetObserverLoading) {
-          return const Center(child: LoadingIndicator());
-        } else if (assetObserverState is AssetObserverFailure) {
-          return const Center(child: Text('Failed to load assets'));
-        } else if (assetObserverState is AssetObserverLoaded && assetObserverState.assets.isNotEmpty) {
-          return BlocBuilder<AssetListBloc, AssetListState>(
-            builder: (context, assetListState) {
-              if (!cachedImagesHaveBeenLoaded) startLoadingCachedImages();
 
-              if (widget.albumId == trashAlbumId) {
-                // sort assets by the date they were deleted
-                assetObserverState.assets.sort((a, b) => a.modifiedAt.compareTo(b.modifiedAt));
-              }
-
-              WidgetsBinding.instance.addPostFrameCallback((_) {
-                scrollController.animateTo(
-                    // scrollController.position.hasContentDimensions
-                    //     ? scrollController.position.maxScrollExtent
-                    //     : 0,
-                    scrollController.position.maxScrollExtent,
-                    duration: const Duration(milliseconds: 300),
-                    curve: Curves.easeOut);
-              });
-
-              return SafeArea(
-                child: Stack(
-                  children: [
-                    Column(
-                      children: [
-                        Expanded(
-                          child: GridView.count(
-                            controller: scrollController,
-                            addAutomaticKeepAlives: true,
-                            crossAxisSpacing: 6,
-                            mainAxisSpacing: 6,
-                            crossAxisCount: 3,
-                            children: assetObserverState.assets.map((asset) {
-                              return AssetListPreviewCard(
-                                asset: asset,
-                                index: assetObserverState.assets.indexOf(asset),
-                                albumId: widget.albumId,
-                                isSelected: assetListState.selectedAssets.contains(asset),
-                              );
-                            }).toList(),
-                          ),
-                        ),
-                        if (widget.albumId == trashAlbumId)
-                          const TrashAssetListBottomMenu()
-                        else
-                          AssetListBottomMenu(albumId: widget.albumId),
-                      ],
-                    ),
-                    if (!cachedImagesHaveBeenLoaded)
-                      Container(
-                        color: themeData.scaffoldBackgroundColor,
-                        constraints: const BoxConstraints.expand(),
-                        child: const Center(
-                          child: LoadingIndicator(),
-                        ),
-                      )
-                  ],
-                ),
-              );
-            },
-          );
-        } else if (assetObserverState is AssetObserverLoaded && assetObserverState.assets.isEmpty) {
-          return Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                const Icon(
-                  CupertinoIcons.photo,
-                  size: 50,
-                ),
-                const SizedBox(height: 8),
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 50),
-                  child: Text(
-                    widget.isTrash ? 'Trash is empty' : 'Add some assets by tapping on the camera button',
-                    textAlign: TextAlign.center,
-                    style: const TextStyle(fontSize: 18),
-                  ),
-                ),
-                const SizedBox(height: 50), // to move the text up a bit
-              ],
-            ),
-          );
-        } else {
-          return Container();
+    return BlocListener<AssetControllerBloc, AssetControllerState>(
+      listener: (context, state) {
+        // scroll to the bottom of the list when the page is first loaded after the controller added assets to the asset-list
+        if (state is AssetControllerInitial) {
+          WidgetsBinding.instance.addPostFrameCallback((_) {
+            scrollController.animateTo(
+                // scrollController.position.hasContentDimensions
+                //     ? scrollController.position.maxScrollExtent
+                //     : 0,
+                scrollController.position.maxScrollExtent,
+                duration: const Duration(milliseconds: 300),
+                curve: Curves.easeOut);
+          });
         }
       },
+      child: BlocBuilder<AssetObserverBloc, AssetObserverState>(
+        builder: (context, assetObserverState) {
+          if (assetObserverState is AssetObserverInitial) {
+            return Container();
+          } else if (assetObserverState is AssetObserverLoading) {
+            return const Center(child: LoadingIndicator());
+          } else if (assetObserverState is AssetObserverFailure) {
+            return const Center(child: Text('Failed to load assets'));
+          } else if (assetObserverState is AssetObserverLoaded && assetObserverState.assets.isNotEmpty) {
+            return BlocBuilder<AssetListBloc, AssetListState>(
+              builder: (context, assetListState) {
+                if (!cachedImagesHaveBeenLoaded) startLoadingCachedImages();
+
+                if (widget.albumId == trashAlbumId) {
+                  // sort assets by the date they were deleted
+                  assetObserverState.assets.sort((a, b) => a.modifiedAt.compareTo(b.modifiedAt));
+                }
+
+                return SafeArea(
+                  child: Stack(
+                    children: [
+                      Column(
+                        children: [
+                          Expanded(
+                            child: GridView.count(
+                              controller: scrollController,
+                              addAutomaticKeepAlives: true,
+                              crossAxisSpacing: 6,
+                              mainAxisSpacing: 6,
+                              crossAxisCount: 3,
+                              children: assetObserverState.assets.map((asset) {
+                                return AssetListPreviewCard(
+                                  asset: asset,
+                                  index: assetObserverState.assets.indexOf(asset),
+                                  albumId: widget.albumId,
+                                  isSelected: assetListState.selectedAssets.contains(asset),
+                                );
+                              }).toList(),
+                            ),
+                          ),
+                          if (widget.albumId == trashAlbumId)
+                            const TrashAssetListBottomMenu()
+                          else
+                            AssetListBottomMenu(albumId: widget.albumId),
+                        ],
+                      ),
+                      if (!cachedImagesHaveBeenLoaded)
+                        Container(
+                          color: themeData.scaffoldBackgroundColor,
+                          constraints: const BoxConstraints.expand(),
+                          child: const Center(
+                            child: LoadingIndicator(),
+                          ),
+                        )
+                    ],
+                  ),
+                );
+              },
+            );
+          } else if (assetObserverState is AssetObserverLoaded && assetObserverState.assets.isEmpty) {
+            return Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Icon(
+                    CupertinoIcons.photo,
+                    size: 50,
+                  ),
+                  const SizedBox(height: 8),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 50),
+                    child: Text(
+                      widget.isTrash ? 'Trash is empty' : 'Add some assets by tapping on the camera button',
+                      textAlign: TextAlign.center,
+                      style: const TextStyle(fontSize: 18),
+                    ),
+                  ),
+                  const SizedBox(height: 50), // to move the text up a bit
+                ],
+              ),
+            );
+          } else {
+            return Container();
+          }
+        },
+      ),
     );
   }
 }
