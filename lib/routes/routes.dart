01d30efe75eb7cdd1320badaa435e20b6861fa45
Finn Evermann <finn.evermann@icloud.com>
2023-06-21 00:34:11 +0200

    remove redundant imports

--- lib/routes/routes.dart
+++ lib/routes/routes.dart
@@ -1,21 +1,4 @@
 import 'package:auto_route/auto_route.dart';
-import 'package:media_vault/features/albums/presentation/album_list_page.dart';
-import 'package:media_vault/features/assets/presentation/asset_carousel/asset_carousel_page.dart';
-import 'package:media_vault/features/assets/presentation/asset_list/asset_list_page.dart';
-import 'package:media_vault/features/assets/presentation/asset_list/trash/trash_page.dart';
-import 'package:media_vault/features/assets/presentation/asset_video_player/asset_video_player_page.dart';
-import 'package:media_vault/features/assets/presentation/move_assets/move_assets_page.dart';
-import 'package:media_vault/features/auth/presentation/local_auth/edit_pin/edit_pin_page.dart';
-import 'package:media_vault/features/auth/presentation/local_auth/enter_local_auth/enter_local_auth_page.dart';
-import 'package:media_vault/features/auth/presentation/local_auth/forgot_pin/forgot_pin_page.dart';
-import 'package:media_vault/features/auth/presentation/local_auth/local_auth_root_page.dart';
-import 'package:media_vault/features/auth/presentation/local_auth/local_auth_setup/confirm_local_auth_setup_page.dart';
-import 'package:media_vault/features/auth/presentation/local_auth/local_auth_setup/local_auth_setup_page.dart';
-import 'package:media_vault/features/auth/presentation/remote_auth/forgot_password/forgot_password_page.dart';
-import 'package:media_vault/features/auth/presentation/remote_auth/login/login_page.dart';
-import 'package:media_vault/features/auth/presentation/remote_auth/register/register_page.dart';
-import 'package:media_vault/other/settings/settings_page.dart';
-import 'package:media_vault/other/splash/splash_page.dart';
 import 'package:media_vault/routes/routes.gr.dart';
 
 // ❯❯ flutter packages pub run build_runner build
@@ -24,7 +7,7 @@ import 'package:media_vault/routes/routes.gr.dart';
 )
 class AppRouter extends $AppRouter {
   @override
-  RouteType get defaultRouteType => RouteType.material();
+  RouteType get defaultRouteType => const RouteType.material();
   @override
   final List<AutoRoute> routes = [
     AutoRoute(page: SplashRoute.page, path: '/'),
